ef2859d6f2b7c569bafca3e969880cd9388cb9c4
Moinul Islam Raj <moinulislamraj0@gmail.com>
2025-11-16 02:16:27 +0600

    added manifest

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("chem-cache-v1").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/styles.css",
+        "/index.js",
+        "/manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
